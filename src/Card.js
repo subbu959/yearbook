bd8f25490765474effacb29412fa4c78b1acedfd
subbu959 <pmohansubhash@gmail.com>
2022-05-28 13:11:00 +0530

    added alt

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Card(props) {
     return(
         <div className="card">
-            <img src={props.item.image} className="card--image" />
+            <img src={props.item.image} className="card--image" alt="profile_pic"/>
             <div className="card-content">
                 <span className="card-location">{props.item.name}</span><br/>
                 <span classname="card-spot">{props.item.rollno}</span>
